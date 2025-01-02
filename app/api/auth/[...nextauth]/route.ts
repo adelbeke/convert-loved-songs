3d3667e179921f601e1aac2302bfb84689508b76
Arthur Delbeke <34245352+adelbeke@users.noreply.github.com>
2025-01-02 22:37:35 +0100

    feat: oauth (#1)
    * chore: add next-auth
    
    * feat: oauth with spotify

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth/next";
+import { type NextAuthOptions } from "next-auth";
+import SpotifyProvider from 'next-auth/providers/spotify';
+
+const providers: NextAuthOptions['providers'] = [
+  SpotifyProvider({
+    authorization:
+      'https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,playlist-modify-private,playlist-modify-public',
+    clientId: process.env.SPOTIFY_CLIENT_ID ?? '',
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET ?? '',
+  }),
+];
+
+const callbacks: NextAuthOptions['callbacks'] = {
+  async jwt({ token, account }) {
+    if (account){
+      token.access_token = account.access_token;
+    }
+    return token;
+  },
+  async session({ session, token }) {
+    return {
+      ...session,
+      token
+    };
+  },
+}
+
+const handler = NextAuth({
+  providers,
+  callbacks
+});
+
+export { handler as GET, handler as POST };
