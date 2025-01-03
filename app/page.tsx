02b334f78f65da64c952e94d6393e5aaf69e82a8
Arthur Delbeke <arthur.delbeke@doctolib.com>
2025-01-03 17:38:09 +0100

    feat: loading page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@
 import { useSession } from "next-auth/react";
 import { ConvertSongs } from "@/features/songs/ConvertSongs/ConvertSongs";
 import { LoginPage } from "@/features/auth/LoginPage";
+import { LoadingPage } from "@/features/auth/LoadingPage";
 
 export default function Home() {
   const { data, status } = useSession();
 
   if (status === "loading") {
-    return <p>Loading...</p>;
+    return <LoadingPage />;
   }
 
   if (status !== "authenticated") {
