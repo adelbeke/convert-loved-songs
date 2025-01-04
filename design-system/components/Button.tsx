3b460d0e91a21dbc73c0cac2862014cfa0d4cbb7
Arthur Delbeke <arthur.delbeke@doctolib.com>
2025-01-04 17:57:20 +0100

    feat: playlist name

--- design-system/components/Button.tsx
+++ design-system/components/Button.tsx
@@ -3,16 +3,18 @@ import { twMerge } from "tailwind-merge";
 
 type Props = PropsWithChildren & {
   className?: string;
+  disabled?: boolean;
   onClick: () => void;
 };
 
-export const Button = ({ children, className, onClick }: Props) => {
+export const Button = ({ children, disabled, className, onClick }: Props) => {
   return (
     <button
       className={twMerge(
         "flex flex-row items-center gap-1 h-10 px-2 rounded-lg bg-transparent shadow-primary shadow-[0_0px_150px_0px_rgba(0,0,0,0.5)] text-sm",
         className,
       )}
+      disabled={disabled}
       onClick={onClick}
     >
       {children}
